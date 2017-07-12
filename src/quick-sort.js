ed101cb5085b57176a419f835bfbbf651b00c455
Naga Pulivarthy <Naga_Pulivarthy@epam.com>
2017-07-12 15:10:06 +0530

    Naga Sindhura Quicksort implementation

--- src/quick-sort.js
+++ src/quick-sort.js
@@ -2,5 +2,49 @@
 
 function quickSort(input) {
 
+if(input == null)
+{
+ throw "Exception";
+}
+
+if(input == 'undefined')
+{
+ return input;
+}
+
+if( !Array.isArray(input) )
+{
+ throw "Exception";
+}
+
+if( input.length == 0 )
+{
+ return input;
+}
+
+
+if(input.length == 1)
+{
+ return input;
+}
+else
+{
+  var left = [];
+		var right = [];
+		var newArray = [];
+		var pivot = input.pop();
+		var length = input.length;
+
+		for (var i = 0; i < length; i++) {
+			if (input[i] <= pivot) {
+				left.push(input[i]);
+			} else {
+				right.push(input[i]);
+			}
+		}
+
+		return newArray.concat(quickSort(left), pivot, quickSort(right));
+    }
+
 };
 module.exports = quickSort;
